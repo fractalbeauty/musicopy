c91e4453535869ef6d3e137f79f3131375add019
hazel <hazel@meows.zip>
2025-12-04 21:45:22 -0800

    bump version to 0.1.6

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -26,14 +26,14 @@ plugins {
 
 val appVersionCode = System.getenv("APP_VERSION_CODE")?.toInt() ?: 1
 
-val appVersion = "0.1.5"
+val appVersion = "0.1.6"
 
 version = appVersion
 val androidVersionName = appVersion
 val desktopVersionName = appVersion
 
-val macosVersionShort = "1.5"
-val macosVersionBuild = "1.5"
+val macosVersionShort = "1.6"
+val macosVersionBuild = "1.6"
 
 buildConfig {
     buildConfigField("APP_VERSION", appVersion)
